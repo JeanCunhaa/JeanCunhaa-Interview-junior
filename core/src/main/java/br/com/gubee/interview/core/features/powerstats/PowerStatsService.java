7fc0a1b8b87e9b7b5ad9367e5837222ec59b9a88
Jean Cunha <jean.cunha13@hotmail.com>
2023-08-31 16:24:34 -0300

    Add PowerStatsService

--- core/src/main/java/br/com/gubee/interview/core/features/powerstats/PowerStatsService.java
+++ core/src/main/java/br/com/gubee/interview/core/features/powerstats/PowerStatsService.java
@@ -17,4 +17,16 @@ public class PowerStatsService {
     public UUID create(PowerStats powerStats) {
         return powerStatsRepository.create(powerStats);
     }
+
+    public PowerStats getPowerStats(UUID id) {
+        return powerStatsRepository.getPowerStats(id);
+    }
+
+    public void delete(UUID powerStatsId) {
+        powerStatsRepository.delete(powerStatsId);
+    }
+
+    public void update(UUID id, PowerStats powerStats) {
+        powerStatsRepository.update(id, powerStats);
+    }
 }
